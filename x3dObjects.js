bffce78b3668d8a6233059e8ab9c46b223851b03
evgeniy-mh <djpon94@gmail.com>
2019-01-07 16:55:29 +0300

    added template code for x3d models list

--- x3dObjects.js
+++ x3dObjects.js
@@ -1,13 +1,62 @@
-var canvas; //ссылка на холст
-var context; //ссылка на контекст холста
-var X3DScene = document.getElementById('X3DScene'); //ссылка на объект x3d
+class X3DModel {
+  constructor(modelURL, modelDescription) {
+    this.modelURL = modelURL;
+    this.modelDescription = modelDescription;
+  }
+}
 
+var modelsListContainer = document.getElementById('modelsListContainer');
 
-//Функия позволяющая быстро задавать необходимые параметры HTML элементов при их создании
+Models = [new X3DModel("x3d/Deer.x3d", "desc"),new X3DModel("x3d/cube.x3d", "desc cube")];
+
+function createX3DModel(model) {
+  console.log(model);
+
+  var inline = document.createElement("inline");
+  inline.setAttribute("url", model.modelURL);
+
+  var scene = document.createElement("scene");
+  scene.setAttribute("id", model.modelURL + "Scene");
+  scene.appendChild(inline);
+
+  var x3d = document.createElement("x3d");
+  setAttributes(x3d, {"width": "400px", "height": "300px"});
+  x3d.appendChild(scene);
+
+  return x3d;
+}
+
+function createX3DModelsList() {
+  Models.forEach(element => {
+    var divWX3DElement = document.createElement("div");
+    divWX3DElement.setAttribute("class", "col");
+    var x3d = createX3DModel(element);
+    //var x3d = document.createTextNode("sdfdsfsd");
+    divWX3DElement.appendChild(x3d);
+
+    var divWModelDescription = document.createElement("div");
+    divWModelDescription.setAttribute("class", "col");
+    var modelDescription = document.createTextNode(element.modelDescription);
+    divWModelDescription.appendChild(
+    document.createTextNode(element.modelDescription));
+
+    var divRow = document.createElement("div");
+    divRow.setAttribute("class", "row");
+    divRow.appendChild(divWX3DElement);
+    divRow.appendChild(divWModelDescription);
+
+    modelsListContainer.appendChild(divRow);
+  });
+}
+
+createX3DModelsList();
+
+//Функия позволяющая быстро задавать необходимые параметры HTML элементов при их
+//создании
 function setAttributes(el, attrs) {
-    for (var key in attrs) {
-        el.setAttribute(key, attrs[key]);
-    }
+  for (var key in attrs) {
+    el.setAttribute(key, attrs[key]);
+  }
 }
 
 //Функция-обработчик события нажатия на кнопку загрузки новой трехмерной модели
@@ -21,14 +70,16 @@ function setAttributes(el, attrs) {
 //     var X3DShape = document.createElement('shape');
 //     var X3DAppearance = document.createElement('appearance');
 //     var X3DMaterial = document.createElement('material');
-//     var texture = X3DAppearance.appendChild(document.createElement("texture"));
+//     var texture =
+//     X3DAppearance.appendChild(document.createElement("texture"));
 //     texture.setAttribute("hideChildren", "false");
 //     var canvas = texture.appendChild(document.createElement("canvas"));
 //     setAttributes(canvas, {
 //         "width": "512",
 //         "height": "512",
 //         "id": "textureCanvas",
-//         "style": "border: solid 1px black; position:absolute; top:20px;left:520px;"
+//         "style": "border: solid 1px black; position:absolute;
+//         top:20px;left:520px;"
 //     });
 
 //     X3DAppearance.appendChild(X3DMaterial);
@@ -45,30 +96,17 @@ function setAttributes(el, attrs) {
 
 //Функция для удаления всем дочерних элементов X3DTransformRoot
 function clearX3DViev() {
-    var X3DTransformRoot = document.getElementById('X3DTransformRoot');
-    for (var i = 0; i < X3DTransformRoot.childNodes.length; i++) {
-        if (X3DTransformRoot.childNodes[i].nodeType === Node.ELEMENT_NODE) {
-            X3DTransformRoot.removeChild(X3DTransformRoot.childNodes[i]);
-            break;
-        }
+  var X3DTransformRoot = document.getElementById('X3DTransformRoot');
+  for (var i = 0; i < X3DTransformRoot.childNodes.length; i++) {
+    if (X3DTransformRoot.childNodes[i].nodeType === Node.ELEMENT_NODE) {
+      X3DTransformRoot.removeChild(X3DTransformRoot.childNodes[i]);
+      break;
     }
+  }
 }
 
 //Функция возвращающая название выбранной трехмерной модели
 function getX3DModel() {
-    return ShapeSelectListBox.options[ShapeSelectListBox.selectedIndex].attributes.name.value;
-}
-
-//Функкия для очистки canvas
-function clearCanvas() {
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    canvas.parentNode._x3domNode.invalidateGLObject();
-
-    clickX = [];
-    clickY = [];
-    clickDrag = [];
-    clickColor = [];
-    clickSize = [];
-    imageTexture = null;
-    redraw();
+  return ShapeSelectListBox.options[ShapeSelectListBox.selectedIndex]
+      .attributes.name.value;
 }
