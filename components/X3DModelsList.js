d10021ce87c92f5e9548320eaa6f72c522a850ac
evgeniy-mh <djpon94@gmail.com>
2019-01-12 08:25:55 +0300

    working on loading next x3d model

--- components/X3DModelsList.js
+++ components/X3DModelsList.js
@@ -2,7 +2,7 @@ const modelsDirectory = "x3d/";
 Models = [
   new X3DModel(modelsDirectory + "cube","Куб"),
   new X3DModel(modelsDirectory + "cone","Конус"),
-  new X3DModel(modelsDirectory + "cylinder","Конус"),
+  new X3DModel(modelsDirectory + "cylinder","Цилиндр"),
   new X3DModel(modelsDirectory + "sphere","Сфера"),
   new X3DModel(modelsDirectory + "torus","Тор"),
 ]; 
--- components/test.js
+++ components/test.js
@@ -6,25 +6,36 @@ function loadPageHeader() {
 }
 loadPageHeader();
 
+//Массив фигур. Содержит фигуры правильное название котторых пользователю
+//нелбходимо будет указать.
+var questions = [];
+//Номер текущего вопроса
+var questionIndex = 0;
+
+//Массив фигур. Содержит правильный ответ и несколько неправильных.
 var answers = [];
+
+//
 var rightAnswer;
+
+(function() { initQuestionsList(); })();
 
 function createX3DModel(model) {
   var inline = document.createElement("inline");
   inline.setAttribute("url", model.getX3DModelName());
 
   var scene = document.createElement("scene");
-  scene.setAttribute("id", model.getX3DModelName() + "Scene");
+  scene.setAttribute("id", model.getX3DModelName());
   scene.appendChild(inline);
 
   var x3d = document.createElement("x3d");
-  setAttributes(x3d, {"width": "400px", "height": "300px"});
+  setAttributes(x3d, {"id": "x3dElement", "width": "400px", "height": "300px"});
   x3d.appendChild(scene);
 
   return x3d;
 }
 
-function showX3DModel(model) {
+function loadX3DContainer(model) {
   var divWX3DElement = document.createElement("div");
   divWX3DElement.setAttribute("class", "col");
   var x3d = createX3DModel(model);
@@ -35,6 +46,10 @@ function showX3DModel(model) {
   divRow.appendChild(divWX3DElement);
 
   modelContainer.appendChild(divRow);
+}
+
+function deleteX3DCurrentModel() {
+  document.getElementById("x3dElement").remove();
 }
 
 //Функия позволяющая быстро задавать необходимые параметры HTML элементов при их
@@ -54,13 +69,15 @@ function initModelAnswers(rightAnswer) {
 
   //Добавить правильный ответ
   this.rightAnswer = rightAnswer;
+  this.answers.length=0;
   this.answers.push(rightAnswer);
 
   //Найти случайные неправильные ответы
   i = 0;
   while (i < numberOfAnswers - 1) {
     var nextIndex = Math.floor(Math.random() * Models.length);
-    if (answers.includes(Models[nextIndex]) || (rightAnswer.name===Models[nextIndex].name)) {
+    if (answers.includes(Models[nextIndex]) ||
+        (rightAnswer.name === Models[nextIndex].name)) {
       continue;
     } else {
       i++;
@@ -74,23 +91,48 @@ function initModelAnswers(rightAnswer) {
   this.answers[0] = this.answers[randIndex];
   this.answers[randIndex] = temp;
 
+  $(answerButtonsContainer).empty();
+
   //Вывести варианты ответов в качестве кнопок
   this.answers.forEach(element => {
     var button = document.createElement("button");
     button.type = "button";
     button.className = "btn btn-secondary";
     button.innerText = element.rusName;
-    button.onclick=checkAnswer;
+    button.onclick = checkAnswer;
     answerButtonsContainer.appendChild(button);
   });
 
-  showX3DModel(this.rightAnswer);
+  //loadX3DContainer(this.rightAnswer);
+  loadX3DContainer(this.rightAnswer);
 }
 
-initModelAnswers(Models[3]);
+function initQuestionsList() {
+  questions = Models.slice(0);
+  shuffleArray(questions);
+  initModelAnswers(questions[questionIndex]);
+}
 
-function checkAnswer(event){
-  if(event.target.innerText===rightAnswer.rusName){
+function checkAnswer(event) {
+  if (event.target.innerText === rightAnswer.rusName) {
     console.log("da");
   }
+}
+
+function nextQuestion() {
+  deleteX3DCurrentModel();
+  questionIndex++;
+  initModelAnswers(questions[questionIndex]);
+}
+
+//Алгоритм Фишера–Йетса для случайной перестановки элементов в массиве
+function shuffleArray(a) {
+  var j, x, i;
+  for (i = a.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    x = a[i];
+    a[i] = a[j];
+    a[j] = x;
+  }
+  return a;
 }
